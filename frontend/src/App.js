785ae774a37580a089c4e99bc9a5efdb491ab8cb
chuma615 <ctuwazurike@gmail.com>
2021-12-27 07:20:26 -0500

    Initial JugoMar app commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome To JugoMar</h1>
+      <h3>Nourishing Nutritionals</h3>
+    </>
+  );
+};
+
+export default App;
