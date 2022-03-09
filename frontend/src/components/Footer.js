98f4d840a94c70faf24fe7295921cbffa5fff9ce
chuma615 <ctuwazurike@gmail.com>
2022-03-09 06:09:33 -0500

    make contact us better

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-const Year = new Date().getFullYear()
+const Year = new Date().getFullYear();
 
 const Footer = () => {
   return (
@@ -8,18 +8,33 @@ const Footer = () => {
       <Container>
         <Row>
           <Col className='text-center py-2'>
-         
-           <ul class="social">    
+            <ul class='social'>
               <li>
-             <h6 className='text-info'>Contact us:</h6>
+                <h6 className='text-info'>Contact us:</h6>
               </li>
               <li>
-             <a href={`mailto:jugomarltd@gmail.com`}>
-                <img src={require('../img/email.png')} alt=""/>jugomarltd@gmail.com</a>
+                <a href={`mailto:jugomarltd@gmail.com`}>
+                  <img src={require('../img/email.png')} alt='' />{' '}
+                  jugomarltd@gmail.com
+                </a>
+              </li>
+              <li>
+                <a href={`https://api.whatsapp.com/send?phone=+2348033395195`}>
+                  <img
+                    src={require('../img/whatsapp.png')}
+                    alt='+234 (803) 339-5195'
+                  />{' '}
+                  +234 (803) 339-5195
+                </a>
               </li>
               <li>
                 <a href={`tel:+2348033395195`}>
-                  <img src={require('../img/whatsapp.png')} alt=""/>+234 (803) 339-5195</a>
+                  <img
+                    src={require('../img/phone.png')}
+                    alt='+234 (803) 339-5195'
+                  />{' '}
+                  +234 (803) 339-5195
+                </a>
               </li>
             </ul>
           </Col>
